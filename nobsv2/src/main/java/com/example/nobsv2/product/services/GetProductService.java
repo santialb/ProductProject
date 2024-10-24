a632066f42cdd9be926fc9f4fbc4a47375f8c3b0
Santiago Albarracin <sa_albarracin@hotmail.com>
2024-10-24 10:44:10 -0500

    Add loggers to two classes and a logging file

--- nobsv2/src/main/java/com/example/nobsv2/product/services/GetProductService.java
+++ nobsv2/src/main/java/com/example/nobsv2/product/services/GetProductService.java
@@ -5,6 +5,8 @@ import com.example.nobsv2.exceptions.ProductNotFoundException;
 import com.example.nobsv2.product.ProductRepository;
 import com.example.nobsv2.product.model.Product;
 import com.example.nobsv2.product.model.ProductDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -16,12 +18,15 @@ public class GetProductService implements Query<Integer, ProductDTO> {
 
     private final ProductRepository productRepository;
 
+    private final static Logger logger = LoggerFactory.getLogger((GetProductService.class));
+
     public GetProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
     @Override
     public ResponseEntity<ProductDTO> execute(Integer input) {
+        logger.info("Executing " + getClass() + " input: " + input);
         Optional<Product> productOptional = productRepository.findById(input);
         if (productOptional.isPresent()){
             return ResponseEntity.ok(new ProductDTO(productOptional.get()));
